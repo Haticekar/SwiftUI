8b4cfcc8132fe6c5241b8d2f8a104bce9660f465
Haticekar <hatticekar14@gmail.com>
2024-11-04 11:11:50 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hatice Kar on 4.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
